e7a8885131beda39adbc50f500b4e88ecd1550dc
muhammad17rabai <muhammad17rabai98@gmail.com>
2025-05-13 22:18:35 +0300

    .ini

--- functions/connect.php
+++ functions/connect.php
@@ -7,10 +7,18 @@ class DB {
         protected $db_pass = '12345678@Almakshoof';
         protected $db_name = 'u101161004_almakshoofs';
     */
-        protected $db_host = 'localhost';
-        protected $db_user = 'u101161004_root';
-        protected $db_pass = '12345678@Almakshoof';
-        protected $db_name = 'u101161004_almakshoofs';
+    protected $db_host;
+    protected $db_user;
+    protected $db_pass;
+    protected $db_name;
+    public function __construct() {
+        $config = include(__DIR__ . '/config.php');
+
+        $this->db_host = $config['DB_HOST'];
+        $this->db_user = $config['DB_USER'];
+        $this->db_pass = $config['DB_PASS'];
+        $this->db_name = $config['DB_NAME'];
+    }
 	public function connect() {
 	
 		$connect_db = new mysqli( $this->db_host, $this->db_user, $this->db_pass, $this->db_name );
