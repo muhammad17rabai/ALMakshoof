ce3579110183867139a0bc636997656e1306c4a8
muhammad17rabai <muhammad17rabai98@gmail.com>
2025-05-13 21:47:31 +0300

    .ini

--- pages/login.php
+++ pages/login.php
@@ -23,16 +23,16 @@
                         <input type="hidden" name="finger" id="finger"/>
                         <div class="form-group">
                             <label><i class="fa fa-user lg text-info">&nbsp;</i>  الايميل أو رقم الهاتف </label>
-                            <input type="text" class="form-control input-lg" name="username" placeholder="الايميل أو رقم الهاتف" required>
+                            <input type="text" class="form-control input-lg" name="username" placeholder="الايميل أو رقم الهاتف" value="<? echo $_POST['username'];?>" required>
                         </div>
                         <div class="form-group">
                             <label><i class="fa fa-lock lg text-info">&nbsp;</i> كلمة المرور </label>
-                            <input type="password" class="form-control input-lg" name="password" placeholder="كلمة السر" required>
+                            <input type="password" class="form-control input-lg" name="password" placeholder="كلمة السر" value="<? echo $_POST['password'];?>" required>
                         </div>
                         <div class="form-group">
                             <a href="index.php?page=Recover_password" class="forgot-pass-link text-info">نسيت كلمة المرور؟</a>
                         </div>
-                        <button type="submit" class="btn btn-block btn-lg btn-info" name="login">تسجيل الدخول</button>
+                        <button type="submit" class="btn btn-block btn-lg btn-info" name="login">تسجيل الدخول jjj</button>
                     </form>
                 </div>
             </div>
