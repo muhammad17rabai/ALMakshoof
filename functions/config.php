308316a0d79165007b9252fa3d7ac1552848099c
muhammad17rabai <muhammad17rabai98@gmail.com>
2025-05-14 23:19:48 +0300

    .ini

--- functions/config.php
+++ functions/config.php
@@ -3,8 +3,8 @@
 
 return [
     'DB_HOST' => 'localhost',
-    'DB_USER' => 'root',
-    'DB_PASS' => '12345678',
-    'DB_NAME' => 'almakshoof',
+    'DB_USER' => 'u101161004_root',
+    'DB_PASS' => '12345678@Almakshoof',
+    'DB_NAME' => 'u101161004_almakshoof',
 ];
 ?>
