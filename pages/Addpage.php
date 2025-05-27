87516206b846271f517808b5cca4d759148e168e
muhammad17rabai <muhammad17rabai98@gmail.com>
2025-05-27 22:23:55 +0300

    .ini

--- pages/Addpage.php
+++ pages/Addpage.php
@@ -23,6 +23,7 @@ $cc = $check_sub->check_subscribe();
         </ul>
     </div>
     <?
+    /*
         if ($role->r('role') == 1) {
             $url = '<a href="index.php?page=subscribe"> من هنا </a>';
             if ($cc == 'not_exit') {
@@ -44,7 +45,8 @@ $cc = $check_sub->check_subscribe();
         }else{
             $cc = 1;
         }
-        if ($cc == 1) {?>
+        if ($cc == 1) {
+        */?>
         <!-- Page Container -->
         <div class="page-container">
             <div class="container">
@@ -127,6 +129,6 @@ $cc = $check_sub->check_subscribe();
             </div>
         </div>
         <!-- End Page Container -->
-    <?}?>
+    <?//}?>
 </main>
 <!-- –––––––––––––––[ END PAGE CONTENT ]––––––––––––––– -->
